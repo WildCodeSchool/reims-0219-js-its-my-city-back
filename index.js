72702ebb9fdb9e5da36778de7eb3b414d1d76deb
Sébastien Jolly <seb_879@hotmail.fr>
2019-06-04 11:46:40 +0200

    correcting for the pull request

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 3001;
 const connection = require('./conf');
 
 const bodyParser = require('body-parser');
@@ -15,72 +15,16 @@ app.use(bodyParser.urlencoded({
 
   //All to pick up a filter for the keywords in the database
   //Test for deployment
-  app.get('/Poi/Filter/', (req, res) => {
-    connection.query('SELECT name FROM test', (err, results) => {
+  app.get('/poi/filter/', (req, res) => {
+    connection.query('SELECT * FROM pointofinterest', (err, results) => {
       if (err) {
-        res.status(500).send('Erreur lors de la récupération des ');
+        res.status(500).send('Erreur lors de la récupération des points d\'intèrêts' );
       } else {
         res.json(results);
       }
     });
   });
 
-/*
-//Allow the creation a point of interest
-app.post('/Poi', (req, res) => {
-    connection.query('SELECT * from ', (err, results) => {
-      if (err) {
-        res.status(500).send('Erreur lors de la récupération des ');
-      } else {
-        res.json(results);
-      }
-    });
-  });
-
-//Random sample of pins for the home page of the app
-app.get('/Poi/Sample', (req, res) => {
-    connection.query('SELECT * from ', (err, results) => {
-      if (err) {
-        res.status(500).send('Erreur lors de la récupération des ');
-      } else {
-        res.json(results);
-      }
-    });
-  });
-
-  
-  //Get the description of a specific point of interest
-  app.get('/Poi/Filter/:id', (req, res) => {
-    connection.query('SELECT * from ', (err, results) => {
-      if (err) {
-        res.status(500).send('Erreur lors de la récupération des ');
-      } else {
-        res.json(results);
-      }
-    });
-  });
-
-  //Post a new user account in the database
-  app.post('/User', (req, res) => {
-    connection.query('SELECT * from ', (err, results) => {
-      if (err) {
-        res.status(500).send('Erreur lors de la récupération des ');
-      } else {
-        res.json(results);
-      }
-    });
-  });
-
-  //Post a new rating of a point of interest
-  app.post('/User/:id/Grade/Poi/:id', (req, res) => {
-    connection.query('SELECT * from ', (err, results) => {
-      if (err) {
-        res.status(500).send('Erreur lors de la récupération des ');
-      } else {
-        res.json(results);
-      }
-    });
-  });*/
 
 app.listen(port, (err) => {
   if (err) {
