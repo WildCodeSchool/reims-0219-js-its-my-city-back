0d61f7f33a7205fbc9ce182c420345d2ae388885
Jeremy Figueiredo <figueiredojeremy@gmail.com>
2019-06-06 11:34:26 +0200

    insert bdd for the router

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 /* eslint-disable import/no-extraneous-dependencies */
 const express = require('express');
+
 const app = express();
 const port = 3001;
 
-const user = require('./routes/user/route');
 const poi = require('./routes/pointofinterest/route');
 
-app.use('/user', user);
-app.use('/poi', poi);
+app.use('/pois', poi);
 
 
 app.listen(port, (err) => {
   if (err) {
     throw new Error(`Something bad happened, server is not listening ${err}`);
-  }else{
-  console.log(`Server is listening on ${port}...`)};
+  } else {
+    console.log(`Server is listening on ${port}...`)
+  }
 });
--- routes/pointofinterest/route.js
+++ routes/pointofinterest/route.js
@@ -6,19 +6,8 @@ router.use((req, res, next) => {
   next()
 });
 
-router.get('/:id', (req, res) => {
-  const poiId = req.params.id
-  connection.query('SELECT * FROM pointofinterest WHERE id=?', [poiId], (err, datas) => {
-    if (err){
-      res.status(500).send(`Erreur lors de la récupération du point d\'interet ${poiId} : ${err}`);
-    }else {
-      res.json(datas)
-    }
-  })
-});
-
 router.get('/sample', function (req, res) {
-  connection.query('SELECT * FROM pointofinterest WHERE id = ROUND( RAND() * 4) + 1', (err, datas) => {
+  connection.query('SELECT * FROM point_of_interest ORDER BY RAND() LIMIT 5', (err, datas) => {
     if (err){
       res.status(500).send(`Erreur lors de la récupération des points d\'interets : ${err}`);
     }else{
@@ -27,22 +16,15 @@ router.get('/sample', function (req, res) {
   }) 
 });
 
-router.post('/create', function (req, res) {
-  connection.query('', (err, datas) => {
+router.get('/:id', (req, res) => {
+  const poiId = req.params.id
+  connection.query('SELECT * FROM point_of_interest WHERE id=?', [poiId], (err, datas) => {
     if (err){
-      res.status(500).send(`Erreur lors de la création du point d\'interet : ${err}`);
-    }else{
-    res.json(datas)
+      res.status(500).send(`Erreur lors de la récupération du point d\'interet ${poiId} : ${err}`);
+    }else {
+      res.json(datas)
     }
   })
-});
-
-router.get('/filter', function (req, res) {
-  if (err){
-    res.status(500).send(`Erreur de lors récupération des filtres : ${err}`);
-  }else{
-    res.json(datas)
-  }
 });
 
 module.exports = router;
