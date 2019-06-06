02cf7ab1066eff00e80e231a56619845ac2ff563
Chandelier Axel <AxeletCana@hotmail.fr>
2019-06-06 14:28:32 +0200

    function to rewritte datas receved from database

---
+++ functions/Add_array.js
@@ -0,0 +1,15 @@
+const addArray = datas => (
+  datas.map(data => ({
+    id: data.id,
+    name: data.name,
+    informations: data.informations,
+    localisation: [
+      data.latitude,
+      data.longitude,
+    ],
+    author_id: data.author_id,
+    picture_id: data.picture_id,
+  }))
+);
+
+module.exports = addArray;
--- routes/pointofinterest/route.js
+++ routes/pointofinterest/route.js
@@ -1,30 +1,33 @@
 const express = require('express');
+
 const router = express.Router();
 const connection = require('../../conf');
+const addArray = require('../../functions/Add_array');
 
 router.use((req, res, next) => {
-  next()
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
 });
 
-router.get('/sample', function (req, res) {
+router.get('/sample', (req, res) => {
   connection.query('SELECT * FROM point_of_interest ORDER BY RAND() LIMIT 5', (err, datas) => {
-    if (err){
-      res.status(500).send(`Erreur lors de la récupération des points d\'interets : ${err}`);
-    }else{
-      res.json(datas)
+    if (err) {
+      res.status(500).send(`Erreur lors de la récupération des points d'interets : ${err}`);
+    } else {
+      res.json(addArray(datas));
     }
-  }) 
+  });
 });
 
 router.get('/:id', (req, res) => {
-  const poiId = req.params.id
+  const poiId = req.params.id;
   connection.query('SELECT * FROM point_of_interest WHERE id=?', [poiId], (err, datas) => {
-    if (err){
-      res.status(500).send(`Erreur lors de la récupération du point d\'interet ${poiId} : ${err}`);
-    }else {
-      res.json(datas)
+    if (err) {
+      res.status(500).send(`Erreur lors de la récupération du point d'interet ${poiId} : ${err}`);
+    } else {
+      res.json(datas);
     }
-  })
+  });
 });
 
 module.exports = router;
