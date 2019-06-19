c30b39b6dd80ddd12eedf6ca9ca813f6af2f3542
Greg <gregoire.suray@gmail.com>
2019-06-19 16:30:01 +0200

    adding data of points of interest

--- migrations/20190604144640-Table-Point_Of_Interest.js
+++ migrations/20190604144640-Table-Point_Of_Interest.js
@@ -1,28 +1,27 @@
-'use strict';
-
-var dbm;
-var type;
-var seed;
+/* eslint-disable func-names */
+let dbm;
+let type;
+let seed;
 
 /**
   * We receive the dbmigrate dependency from dbmigrate initially.
   * This enables us to not have to rely on NODE_PATH.
   */
-exports.setup = function(options, seedLink) {
+exports.setup = function (options, seedLink) {
   dbm = options.dbmigrate;
   type = dbm.dataType;
   seed = seedLink;
 };
 
-exports.up = function(db) {
+exports.up = function (db) {
   return db.createTable('point_of_interest', {
     id: {
       type: 'int', primaryKey: true, autoIncrement: true, notNull: true,
     },
     name: { type: 'char', length: 80, notNull: true },
-    latitude: { type : "float", notNull : true },
-    longitude: { type : "float", notNull : true },
-    creation_date: {type: 'datetime', notNull: true, },
+    latitude: { type: 'float', notNull: true },
+    longitude: { type: 'float', notNull: true },
+    creation_date: { type: 'datetime', notNull: true },
     author_id: {
       type: 'int',
       notNull: true,
@@ -44,164 +43,164 @@ exports.up = function(db) {
       },
     },
   })
-  .then(
-    function(result){
-      db.insert("point_of_interest", [
-        "name",
-        "creation_date",
-        "author_id",
-        "picture_id",
-        "latitude",
-        "longitude"
-      ], [
-        "Piscine Talleyrand",
-        "2019-06-12 08:00:00",
-        "1",
-        "1",
-        "49.2563256",
-        "4.025759"
-      ])
-    }
-  )
-  .then(
-    function(result){
-      db.insert("point_of_interest", [
-        "name",
-        "creation_date",
-        "author_id",
-        "picture_id",
-        "latitude",
-        "longitude"
-      ], [
-        "Skate Park",
-        "2019-06-10 09:00:00",
-        "1",
-        "2",
-        "49.244777",
-        "4.025301"
-      ])
-    }
-  )
-  .then(
-    function(result){
-      db.insert("point_of_interest", [
-        "name",
-        "creation_date",
-        "author_id",
-        "picture_id",
-        "latitude",
-        "longitude"
-      ], [
-        "Terrain de foot",
-        "2019-05-02 08:20:03",
-        "1",
-        "3",
-        "49.276670",
-        "4.019728"
-      ])
-    }
-  )
-  .then(
-    function(result){
-      db.insert("point_of_interest", [
-        "name",
-        "creation_date",
-        "author_id",
-        "picture_id",
-        "latitude",
-        "longitude"
-      ], [
-        "Table de Ping Pong",
-        "2018-05-12 08:00:40",
-        "1",
-        "4",
-        "49.240366",
-        "4.019728"
-      ])
-    }
-  )
-  .then(
-    function(result){
-      db.insert("point_of_interest", [
-        "name",
-        "creation_date",
-        "author_id",
-        "picture_id",
-        "latitude",
-        "longitude"
-      ], [
-        "Table de Ping Pong",
-        "2019-03-01 07:04:10",
-        "1",
-        "5",
-        "49.276670",
-        "4.056639"
-      ])
-    }
-  )
-  .then(
-    function(result){
-      db.insert("point_of_interest", [
-        "name",
-        "creation_date",
-        "author_id",
-        "picture_id",
-        "latitude",
-        "longitude"
-      ], [
-        "Table de Ping Pong",
-        "2019-03-01 07:04:10",
-        "1",
-        "6",
-        "49.243785",
-        "3.989211"
-      ])
-    }
-  )
-  .then(
-    function(result){
-      db.insert("point_of_interest", [
-        "name",
-        "creation_date",
-        "author_id",
-        "picture_id",
-        "latitude",
-        "longitude"
-      ], [
-        "Jardin",
-        "2019-03-01 07:04:10",
-        "1",
-        "7",
-        "49.244871",
-        "3.990526"
-      ])
-    }
-  )
-  .then(
-    function(result){
-      db.insert("point_of_interest", [
-        "name",
-        "creation_date",
-        "author_id",
-        "picture_id",
-        "latitude",
-        "longitude"
-      ], [
-        "Parc",
-        "2019-02-04 07:04:10",
-        "1",
-        "8",
-        "49.248961",
-        "3.997111"
-      ])
-    }
-  )
+    .then(
+      (result) => {
+        db.insert('point_of_interest', [
+          'name',
+          'creation_date',
+          'author_id',
+          'picture_id',
+          'latitude',
+          'longitude',
+        ], [
+          'Piscine Talleyrand',
+          '2019-06-12 08:00:00',
+          '1',
+          '1',
+          '49.2563256',
+          '4.025759',
+        ]);
+      },
+    )
+    .then(
+      (result) => {
+        db.insert('point_of_interest', [
+          'name',
+          'creation_date',
+          'author_id',
+          'picture_id',
+          'latitude',
+          'longitude',
+        ], [
+          'Skate Park',
+          '2019-06-10 09:00:00',
+          '1',
+          '2',
+          '49.244777',
+          '4.025301',
+        ]);
+      },
+    )
+    .then(
+      (result) => {
+        db.insert('point_of_interest', [
+          'name',
+          'creation_date',
+          'author_id',
+          'picture_id',
+          'latitude',
+          'longitude',
+        ], [
+          'Terrain de foot',
+          '2019-05-02 08:20:03',
+          '1',
+          '3',
+          '49.276670',
+          '4.019728',
+        ]);
+      },
+    )
+    .then(
+      (result) => {
+        db.insert('point_of_interest', [
+          'name',
+          'creation_date',
+          'author_id',
+          'picture_id',
+          'latitude',
+          'longitude',
+        ], [
+          'Table de Ping Pong',
+          '2018-05-12 08:00:40',
+          '1',
+          '4',
+          '49.240366',
+          '4.019728',
+        ]);
+      },
+    )
+    .then(
+      (result) => {
+        db.insert('point_of_interest', [
+          'name',
+          'creation_date',
+          'author_id',
+          'picture_id',
+          'latitude',
+          'longitude',
+        ], [
+          'Table de Ping Pong',
+          '2019-03-01 07:04:10',
+          '1',
+          '5',
+          '49.276670',
+          '4.056639',
+        ]);
+      },
+    )
+    .then(
+      (result) => {
+        db.insert('point_of_interest', [
+          'name',
+          'creation_date',
+          'author_id',
+          'picture_id',
+          'latitude',
+          'longitude',
+        ], [
+          'Table de Ping Pong',
+          '2019-03-01 07:04:10',
+          '1',
+          '6',
+          '49.243785',
+          '3.989211',
+        ]);
+      },
+    )
+    .then(
+      (result) => {
+        db.insert('point_of_interest', [
+          'name',
+          'creation_date',
+          'author_id',
+          'picture_id',
+          'latitude',
+          'longitude',
+        ], [
+          'Jardin',
+          '2019-03-01 07:04:10',
+          '1',
+          '7',
+          '49.244871',
+          '3.990526',
+        ]);
+      },
+    )
+    .then(
+      (result) => {
+        db.insert('point_of_interest', [
+          'name',
+          'creation_date',
+          'author_id',
+          'picture_id',
+          'latitude',
+          'longitude',
+        ], [
+          'Parc',
+          '2019-02-04 07:04:10',
+          '1',
+          '8',
+          '49.248961',
+          '3.997111',
+        ]);
+      },
+    );
 };
 
-exports.down = function(db) {
+exports.down = function (db) {
   return db.dropTable('point_of_interest');
 };
 
 exports._meta = {
-  "version": 1
+  version: 1,
 };
--- migrations/20190619122632-addPoisData.js
+++ migrations/20190619122632-addPoisData.js
@@ -1,4 +1,5 @@
-'use strict';
+/* eslint-disable func-names */
+
 
 let dbm;
 let type;
@@ -31,28 +32,203 @@ exports.up = function (db) {
     '9',
     '49.253968',
     '4.034061',
-  ]);
+  ])
+    .then(
+      (result) => {
+        db.insert('point_of_interest', [
+          'name',
+          'latitude',
+          'longitude',
+          'creation_date',
+          'author_id',
+          'picture_id',
+        ], [
+          'Porte de Mars',
+          '49.260615',
+          '4.029992',
+          '2019-06-18',
+          '1',
+          '9',
+        ]);
+      },
+    )
+    .then(
+      (result) => {
+        db.insert('point_of_interest', [
+          'name',
+          'latitude',
+          'longitude',
+          'creation_date',
+          'author_id',
+          'picture_id',
+        ], [
+          'Basilique Saint-Remi',
+          '49.243236',
+          '4.042057',
+          '2019-06-18',
+          '1',
+          '9',
+        ]);
+      },
+    )
+    .then(
+      (result) => {
+        db.insert('point_of_interest', [
+          'name',
+          'latitude',
+          'longitude',
+          'creation_date',
+          'author_id',
+          'picture_id',
+        ], [
+          'Toilettes Publiques',
+          '49.254225',
+          '4.031363',
+          '2019-06-18',
+          '1',
+          '9',
+        ]);
+      },
+    )
+    .then(
+      (result) => {
+        db.insert('point_of_interest', [
+          'name',
+          'latitude',
+          'longitude',
+          'creation_date',
+          'author_id',
+          'picture_id',
+        ], [
+          'Décheterie Reims Europe',
+          '49.246514',
+          '4.069071',
+          '2019-06-18',
+          '1',
+          '9',
+        ]);
+      },
+    )
+    .then(
+      (result) => {
+        db.insert('point_of_interest', [
+          'name',
+          'latitude',
+          'longitude',
+          'creation_date',
+          'author_id',
+          'picture_id',
+        ], [
+          'Décheterie Reims Croix Rouge',
+          '49.219887',
+          '4.018164',
+          '2019-06-18',
+          '1',
+          '9',
+        ]);
+      },
+    )
+    .then(
+      (result) => {
+        db.insert('point_of_interest', [
+          'name',
+          'latitude',
+          'longitude',
+          'creation_date',
+          'author_id',
+          'picture_id',
+        ], [
+          'Benne à verre',
+          '49.257599',
+          '4.019289',
+          '2019-06-18',
+          '1',
+          '9',
+        ]);
+      },
+    )
+    .then(
+      (result) => {
+        db.insert('point_of_interest', [
+          'name',
+          'latitude',
+          'longitude',
+          'creation_date',
+          'author_id',
+          'picture_id',
+        ], [
+          'Conteneur à vêtements',
+          '49.274819',
+          '4.017276',
+          '2019-06-18',
+          '1',
+          '9',
+        ]);
+      },
+    )
+    .then(
+      (result) => {
+        db.insert('point_of_interest', [
+          'name',
+          'latitude',
+          'longitude',
+          'creation_date',
+          'author_id',
+          'picture_id',
+        ], [
+          'Conteneur à vêtements',
+          '49.245993',
+          '4.042758',
+          '2019-06-18',
+          '1',
+          '9',
+        ]);
+      },
+    )
+    .then(
+      (result) => {
+        db.insert('point_of_interest', [
+          'name',
+          'latitude',
+          'longitude',
+          'creation_date',
+          'author_id',
+          'picture_id',
+        ], [
+          'Complexe Multisport',
+          '49.261015',
+          '4.018959',
+          '2019-06-18',
+          '1',
+          '9',
+        ]);
+      },
+    )
+    .then(
+      (result) => {
+        db.insert('point_of_interest', [
+          'name',
+          'latitude',
+          'longitude',
+          'creation_date',
+          'author_id',
+          'picture_id',
+        ], [
+          'Terrains de Football Georges Hebert',
+          '49.282505',
+          '4.',
+          '2019-06-18',
+          '1',
+          '9',
+        ]);
+      },
+    );
 };
 
 exports.down = function (db) {
-  return db.delete('point_of_interest', [
-    'name',
-    'latitude',
-    'longitude',
-    'creation_date',
-    'author_id',
-    'picture_id',
-  ],
-  [
-    'Notre Dame de Reims',
-    '49.253968',
-    '4.034061',
-    '2019-06-18',
-    '1',
-    '9',
-  ]);
+  return null;
 };
 
 exports._meta = {
-  "version": 1
+  version: 1,
 };
