02b2141909c76be6a5191e104d757df6c6b9d12d
Chandelier Axel <AxeletCana@hotmail.fr>
2019-06-20 16:44:11 +0200

    Solved bug

--- queries/getSamplePois.js
+++ queries/getSamplePois.js
@@ -2,12 +2,10 @@ const getSamplePoisInfos = require('./getSamplePoisInfos');
 const getSamplePoisInfosCriteria = require('./getSamplePoisInfosCriteria');
 
 
-const getSamplePois =
-`${getSamplePoisInfos} WHERE keyword.name = 'Sport' ${getSamplePoisInfosCriteria} 
-  UNION (${getSamplePoisInfos} WHERE keyword.name = 'Nature' ${getSamplePoisInfosCriteria})
-  UNION (${getSamplePoisInfos} WHERE keyword.name = 'Attraction' ${getSamplePoisInfosCriteria})
-  UNION (${getSamplePoisInfos} WHERE keyword.name = 'Monument' ${getSamplePoisInfosCriteria})
-  UNION (${getSamplePoisInfos} WHERE keyword.name = 'Utilitaire' ${getSamplePoisInfosCriteria})
-  UNION (${getSamplePoisInfos} WHERE keyword.name = 'Hygiène' ${getSamplePoisInfosCriteria});`;
+const getSamplePois = `${getSamplePoisInfos} WHERE keyword.name = 'Sport & Loisirs' ${getSamplePoisInfosCriteria} 
+  UNION (${getSamplePoisInfos} WHERE keyword.name = 'Nature & Développement Durable' ${getSamplePoisInfosCriteria})
+  UNION (${getSamplePoisInfos} WHERE keyword.name = 'Services Publics' ${getSamplePoisInfosCriteria})
+  UNION (${getSamplePoisInfos} WHERE keyword.name = 'Vie Quotidienne' ${getSamplePoisInfosCriteria})
+  UNION (${getSamplePoisInfos} WHERE keyword.name = 'Transports & Mobilité' ${getSamplePoisInfosCriteria});`;
 
 module.exports = (getSamplePois);
