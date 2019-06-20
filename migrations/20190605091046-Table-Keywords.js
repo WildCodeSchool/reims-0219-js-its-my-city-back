3b609370f9b824d51facef74851b14ac05f7f818
Greg <gregoire.suray@gmail.com>
2019-06-20 11:54:02 +0200

    adding of every keywords of second importance

--- migrations/20190605091046-Table-Keywords.js
+++ migrations/20190605091046-Table-Keywords.js
@@ -90,6 +90,695 @@ exports.up = function (db) {
           'horaires',
         ]);
       },
+    )
+    .then(
+      (result) => {
+        db.insert('keyword', [
+          'name',
+          'importance',
+          'informations',
+        ], [
+          'Boulodrome',
+          '2',
+          'horaires',
+        ]);
+      },
+    )
+    .then(
+      (result) => {
+        db.insert('keyword', [
+          'name',
+          'importance',
+          'informations',
+        ], [
+          'Aire de jeux',
+          '2',
+          'horaires',
+        ]);
+      },
+    )
+    .then(
+      (result) => {
+        db.insert('keyword', [
+          'name',
+          'importance',
+          'informations',
+        ], [
+          'Maison de quartier',
+          '2',
+          'horaires',
+        ]);
+      },
+    )
+    .then(
+      (result) => {
+        db.insert('keyword', [
+          'name',
+          'importance',
+          'informations',
+        ], [
+          'Boîte à livres',
+          '2',
+          'horaires',
+        ]);
+      },
+    )
+    .then(
+      (result) => {
+        db.insert('keyword', [
+          'name',
+          'importance',
+          'informations',
+        ], [
+          'Instrument de musique',
+          '2',
+          'horaires',
+        ]);
+      },
+    )
+    .then(
+      (result) => {
+        db.insert('keyword', [
+          'name',
+          'importance',
+          'informations',
+        ], [
+          'Table de ping-pong',
+          '2',
+          'horaires',
+        ]);
+      },
+    )
+    .then(
+      (result) => {
+        db.insert('keyword', [
+          'name',
+          'importance',
+          'informations',
+        ], [
+          'City stade',
+          '2',
+          'horaires',
+        ]);
+      },
+    )
+    .then(
+      (result) => {
+        db.insert('keyword', [
+          'name',
+          'importance',
+          'informations',
+        ], [
+          'Skatepark',
+          '2',
+          'horaires',
+        ]);
+      },
+    )
+    .then(
+      (result) => {
+        db.insert('keyword', [
+          'name',
+          'importance',
+          'informations',
+        ], [
+          'Terrain de foot',
+          '2',
+          'horaires',
+        ]);
+      },
+    )
+    .then(
+      (result) => {
+        db.insert('keyword', [
+          'name',
+          'importance',
+          'informations',
+        ], [
+          'Street workout',
+          '2',
+          'horaires',
+        ]);
+      },
+    )
+    .then(
+      (result) => {
+        db.insert('keyword', [
+          'name',
+          'importance',
+          'informations',
+        ], [
+          'Sport en groupe',
+          '2',
+          'horaires',
+        ]);
+      },
+    )
+    .then(
+      (result) => {
+        db.insert('keyword', [
+          'name',
+          'importance',
+          'informations',
+        ], [
+          'Terrain de basket',
+          '2',
+          'horaires',
+        ]);
+      },
+    )
+    .then(
+      (result) => {
+        db.insert('keyword', [
+          'name',
+          'importance',
+          'informations',
+        ], [
+          'Parc',
+          '2',
+          'horaires',
+        ]);
+      },
+    )
+    .then(
+      (result) => {
+        db.insert('keyword', [
+          'name',
+          'importance',
+          'informations',
+        ], [
+          'Square',
+          '2',
+          'horaires',
+        ]);
+      },
+    )
+    .then(
+      (result) => {
+        db.insert('keyword', [
+          'name',
+          'importance',
+          'informations',
+        ], [
+          'Composteur',
+          '2',
+          'horaires',
+        ]);
+      },
+    )
+    .then(
+      (result) => {
+        db.insert('keyword', [
+          'name',
+          'importance',
+          'informations',
+        ], [
+          'Jardin partagé',
+          '2',
+          'horaires',
+        ]);
+      },
+    )
+    .then(
+      (result) => {
+        db.insert('keyword', [
+          'name',
+          'importance',
+          'informations',
+        ], [
+          'Incroyables Comestibles',
+          '2',
+          'horaires',
+        ]);
+      },
+    )
+    .then(
+      (result) => {
+        db.insert('keyword', [
+          'name',
+          'importance',
+          'informations',
+        ], [
+          'Aire de pique-nique',
+          '2',
+          'horaires',
+        ]);
+      },
+    )
+    .then(
+      (result) => {
+        db.insert('keyword', [
+          'name',
+          'importance',
+          'informations',
+        ], [
+          'Plan d\'eau',
+          '2',
+          'horaires',
+        ]);
+      },
+    )
+    .then(
+      (result) => {
+        db.insert('keyword', [
+          'name',
+          'importance',
+          'informations',
+        ], [
+          'Monuments',
+          '2',
+          'horaires',
+        ]);
+      },
+    )
+    .then(
+      (result) => {
+        db.insert('keyword', [
+          'name',
+          'importance',
+          'informations',
+        ], [
+          'Patrimoine historique',
+          '2',
+          'horaires',
+        ]);
+      },
+    )
+    .then(
+      (result) => {
+        db.insert('keyword', [
+          'name',
+          'importance',
+          'informations',
+        ], [
+          'Refuge animaux',
+          '2',
+          'horaires',
+        ]);
+      },
+    )
+    .then(
+      (result) => {
+        db.insert('keyword', [
+          'name',
+          'importance',
+          'informations',
+        ], [
+          'Dechetterie',
+          '2',
+          'horaires',
+        ]);
+      },
+    )
+    .then(
+      (result) => {
+        db.insert('keyword', [
+          'name',
+          'importance',
+          'informations',
+        ], [
+          'Toilettes',
+          '2',
+          'horaires',
+        ]);
+      },
+    )
+    .then(
+      (result) => {
+        db.insert('keyword', [
+          'name',
+          'importance',
+          'informations',
+        ], [
+          'Fontaines',
+          '2',
+          'horaires',
+        ]);
+      },
+    )
+    .then(
+      (result) => {
+        db.insert('keyword', [
+          'name',
+          'importance',
+          'informations',
+        ], [
+          'Commissariat',
+          '2',
+          'horaires',
+        ]);
+      },
+    )
+    .then(
+      (result) => {
+        db.insert('keyword', [
+          'name',
+          'importance',
+          'informations',
+        ], [
+          'Mairie',
+          '2',
+          'horaires',
+        ]);
+      },
+    )
+    .then(
+      (result) => {
+        db.insert('keyword', [
+          'name',
+          'importance',
+          'informations',
+        ], [
+          'CRIJ',
+          '2',
+          'horaires',
+        ]);
+      },
+    )
+    .then(
+      (result) => {
+        db.insert('keyword', [
+          'name',
+          'importance',
+          'informations',
+        ], [
+          'Médiathèque',
+          '2',
+          'horaires',
+        ]);
+      },
+    )
+    .then(
+      (result) => {
+        db.insert('keyword', [
+          'name',
+          'importance',
+          'informations',
+        ], [
+          'Annexe mairie',
+          '2',
+          'horaires',
+        ]);
+      },
+    )
+    .then(
+      (result) => {
+        db.insert('keyword', [
+          'name',
+          'importance',
+          'informations',
+        ], [
+          'Mairie mobile',
+          '2',
+          'horaires',
+        ]);
+      },
+    )
+    .then(
+      (result) => {
+        db.insert('keyword', [
+          'name',
+          'importance',
+          'informations',
+        ], [
+          'Bibliobus',
+          '2',
+          'horaires',
+        ]);
+      },
+    )
+    .then(
+      (result) => {
+        db.insert('keyword', [
+          'name',
+          'importance',
+          'informations',
+        ], [
+          'Don du sang',
+          '2',
+          'horaires',
+        ]);
+      },
+    )
+    .then(
+      (result) => {
+        db.insert('keyword', [
+          'name',
+          'importance',
+          'informations',
+        ], [
+          'CAF',
+          '2',
+          'horaires',
+        ]);
+      },
+    )
+    .then(
+      (result) => {
+        db.insert('keyword', [
+          'name',
+          'importance',
+          'informations',
+        ], [
+          'CPAM',
+          '2',
+          'horaires',
+        ]);
+      },
+    )
+    .then(
+      (result) => {
+        db.insert('keyword', [
+          'name',
+          'importance',
+          'informations',
+        ], [
+          'Pôle emploi',
+          '2',
+          'horaires',
+        ]);
+      },
+    )
+    .then(
+      (result) => {
+        db.insert('keyword', [
+          'name',
+          'importance',
+          'informations',
+        ], [
+          'Office du tourisme',
+          '2',
+          'horaires',
+        ]);
+      },
+    )
+    .then(
+      (result) => {
+        db.insert('keyword', [
+          'name',
+          'importance',
+          'informations',
+        ], [
+          'Consigne automatique',
+          '2',
+          'horaires',
+        ]);
+      },
+    )
+    .then(
+      (result) => {
+        db.insert('keyword', [
+          'name',
+          'importance',
+          'informations',
+        ], [
+          'Container à verre',
+          '2',
+          'horaires',
+        ]);
+      },
+    )
+    .then(
+      (result) => {
+        db.insert('keyword', [
+          'name',
+          'importance',
+          'informations',
+        ], [
+          'Poubelles',
+          '2',
+          'horaires',
+        ]);
+      },
+    )
+    .then(
+      (result) => {
+        db.insert('keyword', [
+          'name',
+          'importance',
+          'informations',
+        ], [
+          'Boîte aux lettres',
+          '2',
+          'horaires',
+        ]);
+      },
+    )
+    .then(
+      (result) => {
+        db.insert('keyword', [
+          'name',
+          'importance',
+          'informations',
+        ], [
+          'Collecte de vêtements',
+          '2',
+          'horaires',
+        ]);
+      },
+    )
+    .then(
+      (result) => {
+        db.insert('keyword', [
+          'name',
+          'importance',
+          'informations',
+        ], [
+          'Distributeurs de billets',
+          '2',
+          'horaires',
+        ]);
+      },
+    )
+    .then(
+      (result) => {
+        db.insert('keyword', [
+          'name',
+          'importance',
+          'informations',
+        ], [
+          'Marché',
+          '2',
+          'horaires',
+        ]);
+      },
+    )
+    .then(
+      (result) => {
+        db.insert('keyword', [
+          'name',
+          'importance',
+          'informations',
+        ], [
+          'Canisette',
+          '2',
+          'horaires',
+        ]);
+      },
+    )
+    .then(
+      (result) => {
+        db.insert('keyword', [
+          'name',
+          'importance',
+          'informations',
+        ], [
+          'Poste',
+          '2',
+          'horaires',
+        ]);
+      },
+    )
+    .then(
+      (result) => {
+        db.insert('keyword', [
+          'name',
+          'importance',
+          'informations',
+        ], [
+          'Pharmacie',
+          '2',
+          'horaires',
+        ]);
+      },
+    )
+    .then(
+      (result) => {
+        db.insert('keyword', [
+          'name',
+          'importance',
+          'informations',
+        ], [
+          'Recharge de téléphone',
+          '2',
+          'horaires',
+        ]);
+      },
+    )
+    .then(
+      (result) => {
+        db.insert('keyword', [
+          'name',
+          'importance',
+          'informations',
+        ], [
+          'Nurserie',
+          '2',
+          'horaires',
+        ]);
+      },
+    )
+    .then(
+      (result) => {
+        db.insert('keyword', [
+          'name',
+          'importance',
+          'informations',
+        ], [
+          'Bains douches',
+          '2',
+          'horaires',
+        ]);
+      },
+    )
+    .then(
+      (result) => {
+        db.insert('keyword', [
+          'name',
+          'importance',
+          'informations',
+        ], [
+          'Médecine d\'urgence',
+          '2',
+          'horaires',
+        ]);
+      },
+    )
+    .then(
+      (result) => {
+        db.insert('keyword', [
+          'name',
+          'importance',
+          'informations',
+        ], [
+          'Hopital',
+          '2',
+          'horaires',
+        ]);
+      },
+    )
+    .then(
+      (result) => {
+        db.insert('keyword', [
+          'name',
+          'importance',
+          'informations',
+        ], [
+          'Conseil de quartier',
+          '2',
+          'horaires',
+        ]);
+      },
     );
 };
 
