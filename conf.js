da8aa8e889f9c8c468ac86d05e518537b5039728
Jeremy Figueiredo <figueiredojeremy@gmail.com>
2019-06-28 12:04:49 +0200

    modify rout for upload image

--- conf.js
+++ conf.js
@@ -1,11 +1,12 @@
-const  mysql = require('mysql');
+/* eslint-disable import/no-extraneous-dependencies */
+const mysql = require('mysql');
 require('dotenv').config();
 
-const  connection = mysql.createConnection({
-host : process.env.DB_HOST,
-user : process.env.DB_USER, 
-password : process.env.DB_PASS,
-database : 'itsMyCity', 
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: 'itsMyCity',
 });
 
 module.exports = connection;
